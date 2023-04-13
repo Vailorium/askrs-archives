de662e9187a41ce18ead0a7bcd14b7362d0f9f13
Vailorium <casterthe5587@gmail.com>
2023-04-14 02:03:48 +1200

    Do backend schema update and tests

--- server/src/lib/HeroBuildSchema.ts
+++ server/src/lib/HeroBuildSchema.ts
@@ -5,8 +5,8 @@ const heroBuildSchema = Joi.object({
   buildName: Joi.string().required(),
   uid: Joi.string().optional().allow(''),
   heroIdNum: Joi.number().required(),
-  rarity: Joi.number().required(),
-  merges: Joi.number().required(),
+  rarity: Joi.number().min(1).max(5).required(),
+  merges: Joi.number().min(0).max(10).required(),
   weapon: Joi.number().optional(),
   refine: Joi.number().optional(),
   assist: Joi.number().optional(),
@@ -15,17 +15,50 @@ const heroBuildSchema = Joi.object({
   b: Joi.number().optional(),
   c: Joi.number().optional(),
   s: Joi.number().optional(),
-  resplendent: Joi.boolean().optional(),
-  boon: Joi.number().optional(),
-  bane: Joi.number().optional(),
-  floret: Joi.number().optional(),
-  dragonflowers: Joi.number().optional(),
-  blessing: Joi.number().optional(),
-  summonerSupport: Joi.number().optional(),
+  resplendent: Joi.boolean().required(),
+  boon: Joi
+    .when('merges', {
+      is: 0,
+      then: Joi.when('bane', {
+        is: Joi.number().greater(0),
+        then: Joi.number().min(1),
+        otherwise: Joi.number().min(0),
+      }),
+      otherwise: Joi.number().min(0),
+    })
+    .when(Joi.number().greater(0), {
+      then: Joi.invalid(Joi.ref('bane'), Joi.ref('floret')),
+    })
+    .required(),
+  bane: Joi.number()
+    .when('merges', {
+      is: 0,
+      then: Joi.number().when('boon', {
+        is: Joi.number().greater(0),
+        then: Joi.number().min(1),
+        otherwise: Joi.number().min(0),
+      }),
+      otherwise: Joi.number().valid(0),
+    })
+    .when(Joi.number().greater(0), {
+      then: Joi.invalid(Joi.ref('boon')),
+    })
+    .required(),
+  floret: Joi
+    .number()
+    .min(0)
+    .when(Joi.number().greater(0), {
+      then: Joi.invalid(Joi.ref('boon')),
+    })
+    .required(),
+  dragonflowers: Joi.number().min(0).required(),
+  blessing: Joi.number().min(0).required(),
+  summonerSupport: Joi.number().min(0).required(),
   allySupportTarget: Joi.number().optional(),
-  allySupportLevel: Joi.number().optional(),
+  allySupportLevel: Joi.number().min(0).required(),
   createdAt: Joi.string().optional(),
   updatedAt: Joi.string().optional(),
   __v: Joi.string().optional(),
 });
+
 export default heroBuildSchema;
